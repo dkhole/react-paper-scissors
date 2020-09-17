de8ab89eda8c4f59019d365d05035f1a10e00ace
dkhole <dkhlee419@gmail.com>
2020-09-17 12:29:19 +1000

    Updates

---
+++ precache-manifest.0b3517b204b953ae60905337a32b6465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd427c306752908b0b585f26102ddd9",
+    "url": "/react-paper-scissors/index.html"
+  },
+  {
+    "revision": "d06a77bdf0251c80cfc4",
+    "url": "/react-paper-scissors/static/css/main.9afd4e8a.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/react-paper-scissors/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-paper-scissors/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d06a77bdf0251c80cfc4",
+    "url": "/react-paper-scissors/static/js/main.cf2f6090.chunk.js"
+  },
+  {
+    "revision": "5325903446b03b9866d4",
+    "url": "/react-paper-scissors/static/js/runtime-main.9d4cf437.js"
+  }
+]);
